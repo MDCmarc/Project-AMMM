6e79a0608ddae2a912337f1cbb30afacbd1b3c5e
Marc Diaz Calderon <mmdiazcalderon@gmail.com>
2024-12-05 11:06:20 +0100

    Rename and Remove ignored files and update .gitignore

--- Heuristics/main.py
+++ Heuristics/main.py
@@ -1,15 +1,15 @@
-
 from Parse_Validate import parse_and_validate_input_file
-from GreedySolver import GreedySolver
+from solvers import Greedy, LocalSearch, GRASP
 import os
-import glob 
+import glob
+import time
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(script_dir, "Datasets")
-    file_paths = glob.glob(os.path.join(data_dir, "*.dat"))
-    
-    #custom_path = [os.path.join(data_dir, "project.2.dat")]
+    file_paths = glob.glob(os.path.join(data_dir, "custom*.dat"))
+
+    # custom_path = [os.path.join(data_dir, "custom0.dat")]
 
     for path in file_paths:
         D = None
@@ -19,8 +19,40 @@ if __name__ == "__main__":
         m = []
         D, n, N, d, m = parse_and_validate_input_file(path)
 
+        greedy_solver = Greedy(D, N, n, d, m)
+        local_search_solver = LocalSearch(D, N, n, d, m)
+        GRASP_solver = GRASP(D, N, n, d, m)
+
         print(f"File {path}\n################")
-        solver = GreedySolver(D, N, n, d, m)
-        solver.solve2()
-        solver.print_solution()
+
+        # ------------------ Greedy Solver ------------------
+        print("Greedy:")
+        start_time = time.time()
+        greedy_solution = greedy_solver.Solve()
+        end_time = time.time()
+
+        print(f"\tTime: {end_time - start_time:.2f} seconds")
+        # ------------------ ###### ###### ------------------
+
+        # ------------------ Local Search ------------------
+        print("LocalSearch:")
+        start_time = time.time()
+        if greedy_solution:
+            local_search_sol = local_search_solver.Solve(greedy_solution)
+        else:
+            print("\tCan not do Local Search from an empty solution:")
+        end_time = time.time()
+
+        print(f"\tTime: {end_time - start_time:.2f} seconds")
+        # ------------------ ##### ###### ------------------
+
+        # ---------------------- GRASP ----------------------
+        print("GRASP:")
+        start_time = time.time()
+        grasp_sol = GRASP_solver.Solve()
+        end_time = time.time()
+
+        print(f"\tTime: {end_time - start_time:.2f} seconds")
+        # ---------------------- ##### ----------------------
+
         print("\n")
